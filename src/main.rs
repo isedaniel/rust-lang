fc3420f7c751badc9bd7bc70c37b5a50c6b38d64
Daniel <ise.daniel@gmail.com>
2024-01-22 21:33:29 -0300

    guessing game first version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, World!");
+    println!("Guess the number!");
+
+    println!("Enter your guess");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
