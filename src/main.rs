bedf6ddaeb240b89bdbae15896fa13f3ff53906e
Daniel <ise.daniel@gmail.com>
2024-01-24 19:04:13 -0300

    std::cmp::Ordering added to compare the random number with the user entered number

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let number = rand::thread_rng().gen_range(1..=100);
+    println!("Random number is {number}");
 
-    println!("The secret number is {number}");
+    loop {
+        println!("Enter your guess");
+        let mut guess = String::new();
 
-    println!("Enter your guess");
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Line could not be read");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Line could not be read");
+        let guess: u32 = guess.trim().parse().expect("Please enter a number");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You won!");
+                break;
+            },
+        }
+    }
 }
