18c142abf3d33b638f7154ade2677d427dc450e8
Daniel <ise.daniel@gmail.com>
2024-01-24 19:25:29 -0300

    secret number printing for debug reasons has been removed, so we have the final version

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,10 @@ fn main() {
     println!("Guess the number!");
 
     let number = rand::thread_rng().gen_range(1..=100);
-    println!("Random number is {number}");
 
     loop {
         println!("Enter your guess");
         let mut guess = String::new();
-
         io::stdin()
             .read_line(&mut guess)
             .expect("Line could not be read");
@@ -26,10 +24,10 @@ fn main() {
         match guess.cmp(&number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => { 
+            Ordering::Equal => {
                 println!("You won!");
                 break;
-            },
+            }
         }
     }
 }
