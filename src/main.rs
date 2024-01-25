28e600aca9decf9cfc1c2a0e8d5fde295c3cd782
Daniel <ise.daniel@gmail.com>
2024-01-25 18:33:53 -0300

    shadowing also work on inner scopes

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 fn main() {
     let x = 1;
     println!("x is {x}");
+
+    {
+        let x = x + 1;
+        println!("x + 1 in the inner scope is {x}");
+    }
+
     let x = x + 2;
-    println!("x is {x}");
+    println!("x + 2 after the scope is {x}");
 }
