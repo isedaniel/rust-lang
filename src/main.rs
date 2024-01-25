e43f70d13166ee1dfd7caed08be60b2adf5616f1
Daniel <ise.daniel@gmail.com>
2024-01-25 18:11:21 -0300

    variables in rust are immutable by default

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
 
 fn main() {
-    println!("Guess the number!");
-
-    let number = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        println!("Enter your guess");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Line could not be read");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You won!");
-                break;
-            }
-        }
-    }
+    let x = 1;
+    println!("x is {x}");
+    x += 2;
+    println!("x is {x}");
 }
